7b5c077305270b0883c91785461fe449686c08d3
YusufByn <yusuf.buyukaydin33@gmail.com>
2025-10-29 16:47:38 +0100

    comments commit

--- src/components/pages/AddNumbers.jsx
+++ src/components/pages/AddNumbers.jsx
@@ -2,20 +2,31 @@ import { useState } from 'react'
 import './AddNumbers.css'
 
 function AddNumbers() {
+    // on declare une variable d'état pour le premier nombre    
     const [number1, setNumber1] = useState(0)
+    // on declare une variable d'état pour le deuxième nombre
     const [number2, setNumber2] = useState(0)
+    // on declare une variable d'état pour le résultat
     const [result, setResult] = useState(0)
 
+    // on crée une fonction pour ajouter les deux nombres
     const handleAdd = async () => {
+        // on declare une variable pour envoyer les données au backend
         const table = {firstNumber: number1, secondNumber: number2}
+        // on fetch les données en utilisant l'url de l'api
         const reponse = await fetch('http://localhost:3000/addNumbers', {
+            // on utilise la méthode POST pour envoyer les données
             method: 'POST',
+            // on utilise le header Content-Type pour spécifier que les données sont en json
             headers: {
                 'Content-Type': 'application/json'
             },
+            // on utilise le body pour envoyer les données en json
             body: JSON.stringify(table)
         })
+        // on transforme la réponse en json
         const result = await reponse.json()
+        // on set le résultat dans la variable d'état result
         setResult(result.message)
     }
     return (
--- src/components/pages/PairOrNot.jsx
+++ src/components/pages/PairOrNot.jsx
@@ -2,19 +2,30 @@ import { useState } from 'react'
 import './PairOrNot.css'
 
 function PairOrNot() {
+    // on declare une variable d'état pour le nombre
     const [number, setNumber] = useState(0)
+    // on declare une variable d'état pour le résultat
     const [result, setResult] = useState('')
+    // on crée une fonction pour vérifier si le nombre est pair ou impair
     const handleCheck = async () => {
 
+        // on declare une variable pour envoyer les données au backend
         const table = {number: number}
+        // on fetch les données en utilisant l'url de l'api
         const reponse = await fetch('http://localhost:3000/pairOrNot', {
+            // on utilise la méthode POST pour envoyer les données
             method: 'POST',
+            // on utilise le header Content-Type pour spécifier que les données sont en json
             headers: {
+                // on utilise le header Content-Type pour spécifier que les données sont en json
                 'Content-Type': 'application/json'
             },
+            // on utilise le body pour envoyer les données en json
             body: JSON.stringify(table)
         })
+        // on transforme la réponse en json
         const result = await reponse.json()
+        // on set le résultat dans la variable d'état result
         setResult(result.message)
     }
     return (
